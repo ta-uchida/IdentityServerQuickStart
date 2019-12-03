88e5f2e1800e7fddb1f0c99506b60f61ea3a4a2d
Takayoshi Uchida <takayoshi-uchida@mail.nissan.co.jp>
2019-12-03 23:30:15 +0900

    add Logout

--- src/MvcClient/Controllers/HomeController.cs
+++ src/MvcClient/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace MvcClient.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Logout()
+        {
+            return SignOut("Cookies", "oidc");
+        }
     }
 }
